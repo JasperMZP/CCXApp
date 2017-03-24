d7e7337a493778fb142952ace438da214d7d2962
JasperMao123 <jaspermao@icloud.com>
2017-03-24 22:01:29 +0800

    聊天功能

--- app/src/main/java/com/example/jasper/ccxapp/activity/ChatActivity.java
+++ app/src/main/java/com/example/jasper/ccxapp/activity/ChatActivity.java
@@ -1,13 +1,20 @@
-package com.example.jasper.ccxapp.ui;
+package com.example.jasper.ccxapp.activity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.MediaController;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.VideoView;
 
 import com.example.jasper.ccxapp.R;
 import com.hyphenate.EMCallBack;
@@ -16,6 +23,9 @@ import com.hyphenate.chat.EMClient;
 import com.hyphenate.chat.EMMessage;
 import com.hyphenate.chat.EMTextMessageBody;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -28,8 +38,14 @@ public class ChatActivity extends AppCompatActivity implements EMMessageListener
     private EditText messageText;//待发送的消息
     private Button sendMessage;//发送文字消息按钮
     private Button sendVideoMsg;//发送视频消息按钮
+    private Button playBtn;//播放视频
+    private VideoView videoView;//视频播放器
 
-    final String toChatUsername="jasper";
+    private String toChatUsername;
+
+    private static final int CAPTURE_VIDEO_ACTIVITY_REQUEST_CODE = 200;
+    private Intent intent = null;
+    private Uri fileUri = null;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -40,7 +56,13 @@ public class ChatActivity extends AppCompatActivity implements EMMessageListener
         messageText = (EditText)findViewById(R.id.text_message);
         sendMessage = (Button)findViewById(R.id.send_message);
         sendVideoMsg = (Button) findViewById(R.id.send_video);
+        videoView =(VideoView)findViewById(R.id.video_view);
+        playBtn = (Button) findViewById(R.id.play);
 
+        Intent intent = getIntent();
+        if (intent!=null){
+            toChatUsername = intent.getStringExtra("toChatUsername");
+        }
 
         sendMessage.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -56,16 +78,69 @@ public class ChatActivity extends AppCompatActivity implements EMMessageListener
             }
         });
 
+        playBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+               play();
+            }
+        });
+
     }
 
     public void sendVideo(){
-        /*//videoPath为视频本地路径，thumbPath为视频预览图路径，videoLength为视频时间长度
-        EMMessage message = EMMessage.createVideoSendMessage(videoPath, thumbPath, videoLength, toChatUsername);
-//如果是群聊，设置chattype，默认是单聊
-       // if (chatType == CHATTYPE_GROUP)
-            message.setChatType(ChatType.GroupChat);
+        intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);//create a intent to record video
+        fileUri = getOutputMediaFileUri(); // create a file Uri to save the video
+
+        Log.d("Test1",fileUri.toString());
+
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+        intent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 1);
+        startActivityForResult(intent, CAPTURE_VIDEO_ACTIVITY_REQUEST_CODE);
+
+
+        /*MediaMetadataRetriever mmr = new MediaMetadataRetriever();
+        mmr.setDataSource(fileUri.toString());
+        String duration = mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION);// 播放时长单位为毫秒 
+        //videoPath为视频本地路径，thumbPath为视频预览图路径，videoLength为视频时间长度
+        EMMessage message = EMMessage.createVideoSendMessage(fileUri.toString(), null, Integer.parseInt(duration), toChatUsername);
+        //如果是群聊，设置chattype，默认是单聊
+        // if (chatType == CHATTYPE_GROUP)
+            message.setChatType(EMMessage.ChatType.Chat);
         EMClient.getInstance().chatManager().sendMessage(message);*/
     }
+
+    private void play(){
+        // Uri uri = Uri.parse(Environment.getExternalStorageDirectory().getPath()+"/Test_Movie.m4v");
+        //VideoView videoView = (VideoView)this.findViewById(R.id.video_view);
+        videoView.setMediaController(new MediaController(this));
+        videoView.setVideoURI(fileUri);
+        videoView.start();
+        videoView.requestFocus();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == CAPTURE_VIDEO_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(this, "Video saved to:\n" +data.getData(),Toast.LENGTH_LONG).show();
+                Log.d("Test2",data.getData().toString());
+            }
+        }
+    }
+
+    public static Uri getOutputMediaFileUri() {
+        File picDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File videoFile = new File(picDir.getPath() + File.separator + "VIDEO_"+ timeStamp + ".mp4");
+
+        return Uri.fromFile(videoFile);
+    }
+
+
+
+
 
 
     public void sendText(){
