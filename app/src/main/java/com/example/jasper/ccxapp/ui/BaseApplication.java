3228af9af8f0cbd58007f6ce84c58801ba35a607
JasperMao123 <jaspermao@icloud.com>
2017-04-06 13:10:51 +0800

    导入融云sdk

--- app/src/main/java/com/example/jasper/ccxapp/ui/BaseApplication.java
+++ app/src/main/java/com/example/jasper/ccxapp/ui/BaseApplication.java
@@ -6,6 +6,7 @@ import android.app.Application;
 
 import cn.bmob.v3.Bmob;
 
+
 /**
  * Created by Jasper on 2017/3/21.
  */
@@ -16,7 +17,7 @@ public class BaseApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-
         Bmob.initialize(this, "11c7be8b5fad9ba573d2641f495dbc48");//初始化Bmob
+
     }
 }
