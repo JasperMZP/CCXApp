8595969f500d8252c0b49e81881c6899b153a313
JasperMao123 <jaspermao@icloud.com>
2017-04-07 16:22:29 +0800

    增加注册登陆界面

---
+++ app/src/main/java/com/example/jasper/ccxapp/ui/SignUpActivity.java
@@ -0,0 +1,11 @@
+package com.example.jasper.ccxapp.ui;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Jasper on 2017/4/7.
+ */
+
+public class SignUpActivity extends AppCompatActivity {
+
+}
