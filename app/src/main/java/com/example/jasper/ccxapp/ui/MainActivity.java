5040ad10b982622f06f47ec8b643c8d981d80246
JasperMao123 <jaspermao@icloud.com>
2017-04-25 14:08:11 +0800

    修改预览图片的bug

--- app/src/main/java/com/example/jasper/ccxapp/ui/MainActivity.java
+++ app/src/main/java/com/example/jasper/ccxapp/ui/MainActivity.java
@@ -361,7 +361,7 @@ public class MainActivity extends Activity implements
                             @Override
                             public void onItemClick(View view, int position) {
                                 PhotoPreview.builder()
-                                        .setPhotos(showItem.getShowImagesList())
+                                        .setPhotos(((ShowItemModel) getGroup(groupPosition)).getShowImagesList())
                                         .setCurrentItem(position)
                                         .setShowDeleteButton(false)
                                         .start(MainActivity.this);
